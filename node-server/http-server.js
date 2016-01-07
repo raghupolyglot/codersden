8f074ffeb738d466286ed3f6268e03102d944c88
raghupolyglot <raghu.polyglot@gmail.com>
2016-01-07 20:22:04 +0000

    update

--- node-server/http-server.js
+++ node-server/http-server.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Cloud9\n');
+  res.end('Hello World from Node Server\n');
 }).listen(process.env.PORT);
